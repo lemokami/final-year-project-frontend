aaa1a60d8fbe1dff7091b33b5e18fed735516d7a
lemokami <jesswinw@gmail.com>
2022-06-06 11:50:49 +0530

    feat: dummy view

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,20 @@
+import { Col, Container, Spacer } from '@nextui-org/react';
 import type { NextPage } from 'next';
+import ContentCard from '../components/ContentCard';
 import Navbar from '../components/Navbar';
 
 const Home: NextPage = () => {
   return (
     <div>
       <Navbar />
+      <Container xs>
+        {Array.apply(null, Array(5)).map((_, i) => (
+          <Col key={i}>
+            <Spacer y={1} />
+            <ContentCard />
+          </Col>
+        ))}
+      </Container>
     </div>
   );
 };
